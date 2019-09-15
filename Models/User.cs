87566cccb30f98975d2bea4aece9d17fd70b7bd2
Carlos Sanchez <randomouscrap98@aol.com>
2019-09-15 01:25:54 -0400

    Better user validation error messages

--- Models/User.cs
+++ Models/User.cs
@@ -25,17 +25,15 @@ namespace contentapi.Models
     //This is the user as they sign in (or create account?)
     public class UserCredential
     {
-        [MinLength(3)]
-        [MaxLength(20)]
-        //[Remote("CheckUniqueUsername", "Users", AdditionalFields = "id", ErrorMessage = "Username taken!")]
+        [MinLength(3, ErrorMessage="Username too short Min: 3!")]
+        [MaxLength(20, ErrorMessage="Username too long! Max: 20")]
         public string username {get;set;}
 
         [Required]
-        [MinLength(8)]
+        [MinLength(8, ErrorMessage="Password too short!")]
         public string password {get;set;}
 
         [EmailAddress]
-        //[Remote("CheckUniqueEmail", "Users", AdditionalFields = ErrorMessage = "Email taken!")]
         public string email {get; set;}
     }
 }
