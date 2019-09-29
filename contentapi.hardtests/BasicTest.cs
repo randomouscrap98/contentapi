bc3e092bba59c9710aafa38174506ac10d3d6597
Carlos Sanchez <randomouscrap98@aol.com>
2019-09-29 04:41:24 -0400

    ummm testing is hard?

---
+++ contentapi.hardtests/BasicTest.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+
+namespace contentapi.hardtests
+{
+    public class BasicTest : IClassFixture<WebApplicationFactory<contentapi.Startup>>
+    {
+        private readonly WebApplicationFactory<contentapi.Startup> contentApi;
+
+        public BasicTest(WebApplicationFactory<contentapi.Startup> contentApi)
+        {
+            this.contentApi = contentApi;
+        }
+
+        [Theory]
+        [InlineData("/api/users")]
+        public void Test1(string url)
+        {
+            // Arrange
+            var client = contentApi.CreateClient();
+
+            // Act
+            var response = client.GetAsync(url).Result;
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            //Assert.Equal("application/json; charset=utf-8",
+            //    response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
