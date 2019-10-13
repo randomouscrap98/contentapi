211a7776ed7612648da2758b370b98fc354b7021
Carlos Sanchez <randomouscrap98@aol.com>
2019-10-13 13:27:54 -0400

    Middle of broken tests

--- contentapi.test/Tests/CategoryControllerTest.cs
+++ contentapi.test/Tests/CategoryControllerTest.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using contentapi.Controllers;
 using contentapi.Models;
 using Xunit;
@@ -6,6 +9,8 @@ namespace contentapi.test
 {
     public class CategoryControllerTest : ControllerTestBase<CategoriesController>
     {
+        public const Role CategoryRole = Role.SiteAdministrator;
+
         public CategoryView QuickCategory(string name)
         {
             return new CategoryView()
@@ -28,6 +33,79 @@ namespace contentapi.test
             var category = QuickCategory("My new category");
             var result = instance.Controller.Post(category).Result;
             Assert.True(IsNotAuthorized(result.Result));
+        }
+
+        public CategoryView CreateUniqueCategory(ControllerInstance<CategoriesController> instance, Action<CategoryView> alterCategory = null)
+        {
+            var category = QuickCategory("Category_" + UniqueSection());
+            if(alterCategory != null)
+                alterCategory.Invoke(category);
+            var result = instance.Controller.Post(category).Result;
+            Assert.True(IsSuccessRequest(result));
+            Assert.True(result.Value.name == category.name);
+            return result.Value;
+        }
+
+        [Fact]
+        public void TestCreateCategory()
+        {
+            //By default, our user should not be able to post categories
+            var instance = GetInstance(true, CategoryRole);
+            var category = CreateUniqueCategory(instance);
+        }
+
+        [Fact]
+        public void TestGetCategorySingle()
+        {
+            var instance = GetInstance(true, CategoryRole);
+            var category = CreateUniqueCategory(instance);
+            var result = instance.Controller.GetSingle(category.id).Result;
+            Assert.True(IsSuccessRequest(result));
+            Assert.True(result.Value.id == category.id);
+            Assert.True(result.Value.name == category.name);
+        }
+
+        [Fact]
+        public void TestGetCategoryList()
+        {
+            var instance = GetInstance(true, CategoryRole);
+            var category = CreateUniqueCategory(instance);
+            var category2 = CreateUniqueCategory(instance);
+            var result = instance.Controller.Get(new Services.CollectionQuery()).Result;
+            Assert.True(IsSuccessRequest(result));
+            var collection = (IEnumerable<CategoryView>)result.Value["collection"];
+            Assert.True(collection.Count() >= 2);
+            Assert.Contains(collection, x => x.id == category.id);
+            Assert.Contains(collection, x => x.id == category2.id);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TestCantReadCategorySingle(bool loggedIn)
+        {
+            var instance = GetInstance(true, CategoryRole);
+            var category = CreateUniqueCategory(instance, (c) => c.baseAccess = "CUD");
+
+            var instance2 = GetInstance(loggedIn);
+            var result = instance.Controller.GetSingle(category.id).Result;
+            Assert.False(IsSuccessRequest(result));
+            Assert.Null(result.Value);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TestCantReadCategoryGet(bool loggedIn)
+        {
+            var instance = GetInstance(true, CategoryRole);
+            var category = CreateUniqueCategory(instance, (c) => c.baseAccess = "CUD");
+
+            var instance2 = GetInstance(loggedIn);
+            var result = instance.Controller.Get(new Services.CollectionQuery()).Result;
+            Assert.True(IsSuccessRequest(result));
+            var categories = (IEnumerable<CategoryView>)result.Value["collection"];
+            Assert.DoesNotContain(categories, (c) => c.id == category.id);
         }
     }
 }
