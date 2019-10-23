a75d8a8124173a2e64239ef1b600f25a94b14434
Carlos Sanchez <randomouscrap98@aol.com>
2019-10-23 00:48:47 -0400

    Small publish changes

--- contentapi/publish.sh
+++ contentapi/publish.sh
@@ -1,4 +1,5 @@
-dotnet publish -r linux-x64 -c Release
+dotnet publish -r linux-x64 -c Release 
+# -p:PublishSingleFile=true
 rsync -zz -avh -e "ssh -p 240" "./bin/Release/netcoreapp3.0/linux-x64/publish/" random@oboy.smilebasicsource.com:/storage/random/contentapi
 # Remove this or make it optional soon
 rsync -zz -avh -e "ssh -p 240" "./content.db" random@oboy.smilebasicsource.com:/storage/random/contentapi
