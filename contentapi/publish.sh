3b76c00e8efc950eafeb1a69f584eb10b1b1a52a
Carlos Sanchez <randomouscrap98@aol.com>
2019-11-06 22:58:52 -0500

    Publish adds categories (temporary)

--- contentapi/publish.sh
+++ contentapi/publish.sh
@@ -3,6 +3,11 @@ port=240
 mtype=linux-x64
 pfolder=/storage/random/contentapi
 rsync='rsync -zz -avh -e "ssh -p $port"'
+db=content.db
+cdate='2019-01-01 00:00:00.000000'
+cdateid="select id from entities where createDate='$cdate'"
+dbdo="sqlite3 \"$db\""
+corev=3.0
 # -p:PublishSingleFile=true
 
 hostrsync()
@@ -11,7 +16,9 @@ hostrsync()
 }
 
 dotnet publish -r $mtype -c Release 
-hostrsync "./bin/Release/netcoreapp3.0/$mtype/publish/" "$pfolder"
+hostrsync "./bin/Release/netcoreapp$corev/$mtype/publish/" "$pfolder"
 # Remove this or make it optional soon
-hostrsync "./content.db" "$pfolder"
-ssh $phost -p $port "cd $pfolder; chmod 700 contentapi"
+hostrsync "./$db" "$pfolder"
+ssh $phost -p $port "cd $pfolder; chmod 700 contentapi; \
+ $dbdo \"insert into entities(createDate, status, baseAllow) values ('$cdate', 0, 6)\";\
+ $dbdo \"insert into categoryEntities(entityId, name) values (($cdateid), 'sbs-main')\""
