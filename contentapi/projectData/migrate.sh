4db8578d4f80ae9595a5579008127d4562d271c5
Carlos Sanchez <randomouscrap98@aol.com>
2019-12-08 17:09:21 -0500

    Probably breaking stuff with the db

--- contentapi/projectData/migrate.sh
+++ contentapi/projectData/migrate.sh
@@ -9,12 +9,17 @@ then
 	exit 1
 fi
 
+if [ "$ALREADY" == "" ]
+then
+	echo "WARNING: Completely recreating $OUTPUT"
+	rm -f "$OUTPUT"
+fi
+
 # NOTE: the migrations path can't have spaces! how to do??!?!
 for sql in $MIGRATIONS/*.sql
 do
 	name=`basename $sql`
-	mg="$ALREADY/$name"
-	if [ "$ALREADY" != "" ] && [ -f "$mg" ]
+	if [ "$ALREADY" != "" ] && [ -f "$ALREADY/$name" ]
 	then
 		echo "Skipping $name, already migrated"
 	else
@@ -22,7 +27,7 @@ do
 		if [ "$ALREADY" != "" ]
 		then
 			mkdir -p "$ALREADY"
-			touch "$mg"
+			cp "$sql" "$ALREADY"
 		fi
 	fi
 done
--- contentapi/publish.sh
+++ contentapi/publish.sh
@@ -4,10 +4,12 @@ mtype=linux-x64
 pfolder=/storage/random/contentapi
 rsync='rsync -zz -avh -e "ssh -p $port"'
 db=content.db
-cdate='2019-01-01 00:00:00.000000'
-cdateid="select id from entities where createDate='$cdate'"
-dbdo="sqlite3 \"$db\""
+dfolder=projectData
+# cdate='2019-01-01 00:00:00.000000'
+# cdateid="select id from entities where createDate='$cdate'"
+# dbdo="sqlite3 \"$db\""
 corev=3.0
+cwd=`pwd`
 # -p:PublishSingleFile=true
 
 hostrsync()
@@ -15,10 +17,19 @@ hostrsync()
    rsync -zz -avh -e "ssh -p $port" "$1" "$phost:$2"
 }
 
+# This part might not be necessary soon, idk. Once you stop always overwriting, 
+# this should become ok.
+cd "$dfolder"
+./extractSchema.sh
+cd "$cwd"
+
+# The project itself. Note that building/etc. puts basically everything you need
+# into the destination folder (such as language files, dbmigrations, etc). Hooraaayyy.
 dotnet publish -r $mtype -c Release 
 hostrsync "./bin/Release/netcoreapp$corev/$mtype/publish/" "$pfolder"
-# Remove this or make it optional soon
-hostrsync "./$db" "$pfolder"
+
+hostrsync "$dfolder/dbmigrations" "$pfolder"
+hostrsyng "$dfolder/migrate.sh" "$
 ssh $phost -p $port "cd $pfolder; chmod 700 contentapi; \
  $dbdo \"insert into entities(createDate, status, baseAllow) values ('$cdate', 0, 6)\";\
  $dbdo \"insert into categoryEntities(entityId, name) values (($cdateid), 'sbs-main')\""
