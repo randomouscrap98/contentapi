a333cb1d418e9eddd90a4b3c9dd3b04e77e14504
Carlos Sanchez <randomouscrap98@aol.com>
2020-04-04 20:06:46 -0400

    Getting startup setup

--- contentapi/Startup.cs
+++ contentapi/Startup.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using contentapi.Services;
+using contentapi.Services.Implementations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace contentapi
 {
@@ -22,10 +27,50 @@ namespace contentapi
 
         public IConfiguration Configuration { get; }
 
+        public T AddSingletonConfig<T>(IConfiguration configuration, IServiceCollection services, string key) where T : class, new()
+        {
+            T config = new T();
+            configuration.Bind(key, config);
+            services.AddSingleton(config);
+            return config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var dataSection = Configuration.GetSection("Data");
+            //ContentConString = dataSection.GetValue<string>("ContentConnectionString")
+
+            AddSingletonConfig<EmailConfig>(Configuration, services, "Email");
+            AddSingletonConfig<LanguageConfig>(Configuration, services, "Language");
+            var tokenConfig = AddSingletonConfig<TokenServiceConfig>(Configuration, services, "Token");
+            services.AddSingleton(new HashConfig());    //Just use defaults
+
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<ILanguageService, LanguageService>();
+            services.AddTransient<ITokenService, TokenService>();
+            services.AddTransient<IHashService, HashService>();
+
+            services.AddCors();
             services.AddControllers();
+
+            //This is all that JWT junk. I hope it still works like this... I just copied this from my core 2.0 project
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfig.SecretKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,10 +81,15 @@ namespace contentapi
                 app.UseDeveloperExceptionPage();
             }
 
+            //Wide open for now, this might need to be changed later.
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            //Apparently authentication has to come before authorization
+            app.UseAuthentication();    
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
