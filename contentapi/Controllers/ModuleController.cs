ebd02d0d6454f0f6af2c32f8ee642505ab00fd8f
Carlos Sanchez <randomouscrap98@aol.com>
2020-07-02 23:16:46 -0400

    Fleshing out module system

--- contentapi/Controllers/ModuleController.cs
+++ contentapi/Controllers/ModuleController.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using contentapi.Services;
 using contentapi.Services.Implementations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -19,14 +24,28 @@ namespace contentapi.Controllers
             public Dictionary<string, string> saveData = new Dictionary<string, string>();
         }
 
+        public class ModuleMessage
+        {
+            private static long GlobalId = 0;
+
+            public DateTime date {get;set;} = DateTime.Now;
+            public long id = Interlocked.Increment(ref GlobalId);
+            public string message {get;set;}
+            public string module {get;set;}
+            public long uid = -1;
+        }
+
         public static Dictionary<string, TempModule> modules = new Dictionary<string, TempModule>();
+        public static List<ModuleMessage> moduleMessages = new List<ModuleMessage>();
 
         protected UserViewService service;
+        protected IPermissionService permService;
 
-        public ModuleController(ILogger<BaseSimpleController> logger, UserViewService service) 
+        public ModuleController(ILogger<BaseSimpleController> logger, UserViewService service, IPermissionService permissionService) 
             : base(logger) 
         { 
             this.service = service;
+            this.permService = permissionService;
         }
 
         [HttpGet] 
@@ -44,26 +63,67 @@ namespace contentapi.Controllers
             return modules[name].code;
         }
 
+        //private void AddMessage(long uid, string message)
+        //{
+        //    moduleMessages.Add
+        //}
+
         [HttpPost("{name}")]
-        public ActionResult Post([FromRoute]string name, [FromBody]string code)
+        public ActionResult Post([FromRoute]string name, [FromBody]string code) //NOTE: CODE NOT ACTUALLY USED!!! JUST FOR DANG MUSTACHE!!
         {
+            if(!permService.IsSuper(GetRequesterNoFail()))
+                return Unauthorized("Not allowed to post code for modules");
+
             if(!modules.ContainsKey(name))
                 modules.Add(name, new TempModule());
-            
+
             modules[name].code = code;
 
             var script = new Script();
-            script.DoString(code);          //This could take a LONG time.
-            //script.Globals["GetUser"] = (Func<int, Table>)
+            script.DoString(modules[name].code);          //This could take a LONG time.
+            script.Globals["data"] = modules[name].saveData;
+            script.Globals["sendmessage"] = new Action<long, string>((uid, message) =>
+            {
+                moduleMessages.Add(new ModuleMessage()
+                {
+                    uid = uid,
+                    module = name,
+                    message = message
+                });
+            });
             modules[name].script = script;
 
             return Ok();
         }
 
-        //[HttpGet("{name}/{command}")]
-        //public ActionResult<string> RunCommand([FromRoute]string name, [FromRoute]string command, [FromQuery]string data)
-        //{
+        //This is a TEST endpoint!!!
+        [HttpGet("allmessages")]
+        public ActionResult<List<ModuleMessage>> AllMessages()
+        {
+            return moduleMessages;
+        }
 
-        //}
+        [HttpGet("{name}/{command}")]
+        public ActionResult RunCommand([FromRoute]string name, [FromRoute]string command, [FromQuery]string data)
+        {
+            if(!modules.ContainsKey(name))
+                return BadRequest($"No module with name {name}");
+            
+            var mod = modules[name];
+            var cmdfuncname = $"command_{command}";
+            var requester = GetRequesterNoFail();
+
+            DynValue res = mod.script.Call(mod.script.Globals[cmdfuncname], requester.userId, data);
+            
+            //try
+            //{
+            //}
+            //catch(Exception ex)
+            //{
+            //    return BadRequest($"Command failed: {ex.Message}");
+            //}
+
+            return Ok(res.String);
+        }
     }
 }
