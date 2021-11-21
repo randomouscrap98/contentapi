3fd1b42b87d40cfcf0f999bc984d7c43ba10e6e5
Carlos Sanchez <randomouscrap98@aol.com>
2021-11-21 17:11:10 -0500

    Fixed publish script, it publishes to oboy!

--- Deploy/publish.sh
+++ Deploy/publish.sh
@@ -1,7 +1,5 @@
 # General publish script for dotnet core projects
 
-cd ..
-
 name="contentapi"
 pfolder="/var/www/${name}"                  # The REMOTE location to PLACE all files
 phost=publisher@oboy.smilebasicsource.com   # The default server (development) (and user to connect)
@@ -25,35 +23,39 @@ removefiles="ignore uploads content.db dbMigrations/*.done"
 # copyfolders="$deploy/LanguageFiles $deploy/dbmigrate.sh $projectdata/dbMigrations"
 
 # Stuff for dotnet
+projectfolder=${name}
 mtype=linux-x64         # The architecture of the target machine
 corev=6.0               # The version of dotnet core we're using
-projfile=${name}.csproj # The project file for version updating / etc
+projfile=${projectfolder}/${name}.csproj # The project file for version updating / etc
 bconfig=Release         # Build configuration (if you have one)
+lpfolder="${name}/bin/${bconfig}/net$corev/$mtype/publish/"   # The LOCAL location to retrieve binaries
 
 postinstallscript="postinstall.sh"
 postinstallargs=""
 
-# My places!
-lpfolder="./bin/${bconfig}/net$corev/$mtype/publish/"   # The LOCAL location to retrieve binaries
-
-echo "Publishing to $phost:$pfolder"
-
+# Function for syncing to the globally configured location
 hostrsync()
 {
    rsync -zz -${rdf}vh -e "ssh -p $port" "$1" "$phost:$2"
 }
 
+echo "Publishing to $phost:$pfolder"
+
+# Work from the root folder!
+cd ..
+
 # Increment the last digit of the "version" xml (yeah yeah regex whatever, I know what's in csproj)
 sed -i "s/<Version>[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]/&@/g;:a {s/0@/1/g;s/1@/2/g;s/2@/3/g;s/3@/4/g;s/4@/5/g;s/5@/6/g;s/6@/7/g;s/7@/8/g;s/8@/9/g;s/9@/@0/g;t a};s/@/1/g" ${projfile}
 
-# The project itself. Delete the old folder (probably).
 # We REMOVE the local publish folder to get rid of old, no longer needed files
 # (don't want to sync unnecessary or even harmful things).
 # A dotnet publish SHOULD do everything required to make the product. It just
 # doesn't include our personal extras (it probably could though)
 rm -rf "$lpfolder"
+
+# This next stuff needs to operate in the actual app folder
 # WARN: DON'T PUBLISH SINGLE! IT FILLS THE TEMP DIRECTORY!!
-dotnet publish -r $mtype --self-contained -c ${bconfig}
+(cd ${name} && dotnet publish -r $mtype --self-contained -c ${bconfig})
 
 # Copy desired files into publish folder
 for cpyfl in $copyfolders
