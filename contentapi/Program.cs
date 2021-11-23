614769fd0b625597d308fc42bd7a969e43c4ee30
Carlos Sanchez <randomouscrap98@aol.com>
2021-11-23 05:02:47 -0500

    Cookies, but I'm ditching them

--- contentapi/Program.cs
+++ contentapi/Program.cs
@@ -2,14 +2,12 @@ using contentapi.AutoMapping;
 using contentapi.Db;
 using contentapi.Implementations;
 using contentapi.Setup;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-// System.Text STILL does not do what I want it to do
-builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddAutoMapper(typeof(ContentSnapshotProfile)); //You can pick ANY profile, it just needs some type from the binary
 
 // We only use defaults for our regular runtime stuff! Overriding defaults is for testing
@@ -21,6 +19,31 @@ DefaultSetup.AddConfigBinding<GenericSearcherConfig>(builder.Services, builder.C
 builder.Services.AddTransient<ContentApiDbConnection>(ctx => 
     new ContentApiDbConnection(new SqliteConnection(builder.Configuration.GetConnectionString("contentapi"))));
 
+builder.Services.AddCors();
+builder.Services.AddAuthentication(options => { 
+    options.DefaultScheme = "Cookies"; 
+}).AddCookie("Cookies", options => {
+    options.Cookie.Name = "sbs_authcookie";
+    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.Events = new CookieAuthenticationEvents
+    {                          
+        //When no cookie, don't want to redirect with 302 (default), want 401 unauthorized
+        OnRedirectToLogin = redirectContext =>
+        {
+            redirectContext.HttpContext.Response.StatusCode = 401;
+            return Task.CompletedTask;
+        }
+    };
+});                
+
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//    .AddCookie(options => {
+//
+//    });
+
+// System.Text STILL does not do what I want it to do
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,8 +56,32 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .AllowAnyHeader();
+});
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseCookiePolicy(new CookiePolicyOptions
+{
+    //CheckConsentNeeded = _ => true,
+    HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.None,
+    MinimumSameSitePolicy = SameSiteMode.Lax,
+    Secure = CookieSecurePolicy.SameAsRequest,
+    //OnAppendCookie = (context) =>
+    //{
+    //    context.IssueCookie = true;
+    //},
+    //OnDeleteCookie = (context) =>
+    //{
+    //}
+});
 
 app.MapControllers();
 
