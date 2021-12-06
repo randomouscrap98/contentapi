5979db33a72eb8ec2bc18cb572050fb99f2de7d9
Carlos Sanchez <randomouscrap98@aol.com>
2021-12-06 02:41:54 -0500

    Completed user avatar hash change

--- OLDSYSTEM/contentapi/NewSys/AutoMappers/UserProfile.cs
+++ OLDSYSTEM/contentapi/NewSys/AutoMappers/UserProfile.cs
@@ -7,8 +7,9 @@ namespace contentapi
     {
         public UserProfile()
         {
-            CreateMap<UserViewFull, Db.User_Convert>().ForMember(x => x.hidelist, 
-                 opt => opt.MapFrom(src => string.Join(",", src.hidelist)));
+            CreateMap<UserViewFull, Db.User_Convert>()
+                .ForMember(x => x.hidelist, opt => opt.MapFrom(src => string.Join(",", src.hidelist)))
+                .ForMember(x => x.avatar, opt => opt.MapFrom(src => src.avatar.ToString()));
         }
     }
 }
