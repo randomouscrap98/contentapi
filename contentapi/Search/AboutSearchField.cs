cb589c9b07815229802c20d7e0ed13ac1b5b4c93
Carlos Sanchez <randomouscrap98@aol.com>
2022-01-31 00:55:48 -0500

    Using silly functions to generate types

--- contentapi/Search/AboutSearchField.cs
+++ contentapi/Search/AboutSearchField.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+using System.Reflection;
 using AutoMapper;
 
 namespace contentapi.Search;
@@ -34,16 +36,100 @@ public class AboutSearchField
     /// <value></value>
     public bool writableOnUpdate {get;set;} = false;
 
+    public bool nullable {get;set;} = false;
+
     public string type {get;set;} = "unknown";
 }
 
 public class AboutSearchFieldProfile : Profile
 {
+    //This should not be here! Or at least unit test this directly!
+    public string TypeToAboutType(Type t)
+    {
+        if(t == typeof(int) || t == typeof(long) || t == typeof(int?) || t == typeof(long?))
+            return "int";
+        if(t == typeof(string))
+            return "string";
+        if(t == typeof(DateTime) || t == typeof(DateTime?))
+            return "datetime";
+        if(t.IsGenericType)
+        {
+            var genargs = t.GetGenericArguments();
+            if(t.GetGenericTypeDefinition() == typeof(List<>))
+                return $"list[{TypeToAboutType(genargs[0])}]";
+            if(t.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+                return $"keyvalue[{TypeToAboutType(genargs[0])},{TypeToAboutType(genargs[1])}]";
+        } 
+        if(t == typeof(bool))
+            return "bool";
+        if(t.IsEnum)
+            return "string(enum)";
+
+        return "unknown";
+    }
+
+    //All taken from https://stackoverflow.com/a/58454489/1066474
+    public static bool IsNullable(PropertyInfo property) =>
+        IsNullableHelper(property.PropertyType, property.DeclaringType, property.CustomAttributes);
+
+    public static bool IsNullable(FieldInfo field) =>
+        IsNullableHelper(field.FieldType, field.DeclaringType, field.CustomAttributes);
+
+    public static bool IsNullable(ParameterInfo parameter) =>
+        IsNullableHelper(parameter.ParameterType, parameter.Member, parameter.CustomAttributes);
+
+    private static bool IsNullableHelper(Type memberType, MemberInfo? declaringType, IEnumerable<CustomAttributeData> customAttributes)
+    {
+        if (memberType.IsValueType)
+            return Nullable.GetUnderlyingType(memberType) != null;
+
+        var nullable = customAttributes
+            .FirstOrDefault(x => x.AttributeType.FullName == "System.Runtime.CompilerServices.NullableAttribute");
+        if (nullable != null && nullable.ConstructorArguments.Count == 1)
+        {
+            var attributeArgument = nullable.ConstructorArguments[0];
+            if (attributeArgument.ArgumentType == typeof(byte[]))
+            {
+                var args = (ReadOnlyCollection<CustomAttributeTypedArgument>)attributeArgument.Value!;
+                if (args.Count > 0 && args[0].ArgumentType == typeof(byte))
+                {
+                    return (byte)args[0].Value! == 2;
+                }
+            }
+            else if (attributeArgument.ArgumentType == typeof(byte))
+            {
+                return (byte)attributeArgument.Value! == 2;
+            }
+        }
+
+        for (var type = declaringType; type != null; type = type.DeclaringType)
+        {
+            var context = type.CustomAttributes
+                .FirstOrDefault(x => x.AttributeType.FullName == "System.Runtime.CompilerServices.NullableContextAttribute");
+            if (context != null &&
+                context.ConstructorArguments.Count == 1 &&
+                context.ConstructorArguments[0].ArgumentType == typeof(byte))
+            {
+                return (byte)context.ConstructorArguments[0].Value! == 2;
+            }
+        }
+
+        // Couldn't find a suitable attribute
+        return false;
+    }
+
+    //public bool IsNullable(Type t)
+    //{
+    //    return Nullable.GetUnderlyingType(t) != null || t == typeof(string?); //!src.fieldType.IsValueType || Activator.CreateInstance(src.fieldType) == null));
+    //}
+
     public AboutSearchFieldProfile()
     {
         this.CreateMap<DbFieldInfo, AboutSearchField>()
-            .ForMember(dest => dest.type, opt => opt.MapFrom(src => src.fieldType.Name))
+            .ForMember(dest => dest.type, opt => opt.MapFrom(src => TypeToAboutType(src.fieldType)))
             .ForMember(dest => dest.writableOnInsert, opt => opt.MapFrom(src => src.onInsert == WriteRuleType.None))
-            .ForMember(dest => dest.writableOnUpdate, opt => opt.MapFrom(src => src.onUpdate == WriteRuleType.None));
+            .ForMember(dest => dest.writableOnUpdate, opt => opt.MapFrom(src => src.onUpdate == WriteRuleType.None))
+            .ForMember(dest => dest.nullable, opt => opt.MapFrom(src => IsNullable(src.rawProperty!)));
+            //Nullable.GetUnderlyingType(src.fieldType) != null || src.fieldType == ));//!src.fieldType.IsValueType || Activator.CreateInstance(src.fieldType) == null));
     }
 }
