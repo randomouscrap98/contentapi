7325a2718b7629424313230e80ff43f7fa660983
Carlos Sanchez <randomouscrap98@aol.com>
2022-07-07 16:28:49 -0400

    Working(?) write controller generic

--- contentapi/Controllers/WriteController.cs
+++ contentapi/Controllers/WriteController.cs
@@ -1,10 +1,8 @@
 using contentapi.Main;
 using contentapi.Search;
-using contentapi.Utilities;
 using contentapi.data.Views;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using contentapi.data;
 
 namespace contentapi.Controllers;
 
@@ -22,85 +20,32 @@ public class WriteController : BaseController
     }
 
     [HttpPost("message")]
-    public Task<ActionResult<MessageView>> WriteMessageAsync([FromBody]MessageView message)
-    {
-        return MatchExceptions(async () => 
-        {
-            RateLimit(RateWrite);
-            if(message.module != null)
-                throw new ForbiddenException("You cannot create module messages yourself!");
-
-            if(message.receiveUserId != 0)
-                throw new ForbiddenException("Setting receiveUserId in a comment is not supported right now!");
-
-            return await writer.WriteAsync(message, GetUserIdStrict());
-        }); //message used for activity and such
-    }
+    public Task<ActionResult<MessageView>> WriteMessageAsync([FromBody]MessageView message) =>
+        MatchExceptions(() => WriteAsync(message));
 
     [HttpPost("content")]
-    public Task<ActionResult<ContentView>> WriteContentAsync([FromBody]ContentView page, [FromQuery]string? activityMessage) 
-    {
-        return MatchExceptions(async () => 
-        {
-            RateLimit(RateWrite);
-            //THIS IS AWFUL! WHAT TO DO ABOUT THIS??? Or is it fine: files ARE written by the controllers after all...
-            //so maybe it makes sense for the controllers to control this aspect as well
-            if(page.id == 0 && page.contentType == InternalContentType.file)
-                throw new ForbiddenException("You cannot create files through this endpoint! Use the file controller!");
-
-            return await writer.WriteAsync(page, GetUserIdStrict(), activityMessage);
-        }); //message used for activity and such
-    }
+    public Task<ActionResult<ContentView>> WriteContentAsync([FromBody]ContentView page, [FromQuery]string? activityMessage) =>
+        MatchExceptions(() => WriteAsync(page, null, activityMessage));
 
     //This is SLIGHTLY special in that user writes are mostly for self updates... but might be used for new groups as well? You also
     //can't update PRIVATE data through this endpoint
     [HttpPost("user")]
-    public Task<ActionResult<UserView>> WriteUserAsync([FromBody]UserView user)
-    {
-        return MatchExceptions(async () => 
-        {
-            RateLimit(RateWrite);
-            return await writer.WriteAsync(user, GetUserIdStrict()); //message used for activity and such
-        });
-    }
+    public Task<ActionResult<UserView>> WriteUserAsync([FromBody]UserView user) =>
+        MatchExceptions(() => WriteAsync(user));
 
     [HttpPost("watch")]
-    public Task<ActionResult<WatchView>> WriteWatchAsync([FromBody]WatchView watch)
-    {
-        return MatchExceptions(async () => 
-        {
-            RateLimit(RateInteract); //A different rate for watches
-            return await writer.WriteAsync(watch, GetUserIdStrict()); //message used for activity and such
-        });
-    }
+    public Task<ActionResult<WatchView>> WriteWatchAsync([FromBody]WatchView watch) =>
+        MatchExceptions(() => WriteAsync(watch));
 
     [HttpPost("vote")]
-    public Task<ActionResult<VoteView>> WriteVoteAsync([FromBody]VoteView vote)
-    {
-        return MatchExceptions(async () => 
-        {
-            RateLimit(RateInteract); //A different rate for watches
-            return await writer.WriteAsync(vote, GetUserIdStrict()); //message used for activity and such
-        });
-    }
+    public Task<ActionResult<VoteView>> WriteVoteAsync([FromBody]VoteView vote) =>
+        MatchExceptions(() => WriteAsync(vote));
 
     [HttpPost("uservariable")]
-    public Task<ActionResult<UserVariableView>> WriteUserVariableAsync([FromBody]UserVariableView variable)
-    {
-        return MatchExceptions(async () => 
-        {
-            RateLimit(RateUserVariable);
-            return await writer.WriteAsync(variable, GetUserIdStrict()); //message used for activity and such
-        });
-    }
+    public Task<ActionResult<UserVariableView>> WriteUserVariableAsync([FromBody]UserVariableView variable) =>
+        MatchExceptions(() => WriteAsync(variable));
 
     [HttpPost("ban")]
-    public Task<ActionResult<BanView>> WriteBanVariableAsync([FromBody]BanView ban)
-    {
-        return MatchExceptions(async () => 
-        {
-            //RateLimit(RateUserVariable);
-            return await writer.WriteAsync(ban, GetUserIdStrict()); //message used for activity and such
-        });
-    }
+    public Task<ActionResult<BanView>> WriteBanVariableAsync([FromBody]BanView ban) =>
+        MatchExceptions(() => WriteAsync(ban));
 }
