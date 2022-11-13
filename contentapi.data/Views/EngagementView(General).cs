bc948f54539fd6c459057c222288ed4abd449960
Carlos Sanchez <randomouscrap98@aol.com>
2022-11-13 01:19:20 -0500

    Somehow it works, i'm concerned

--- contentapi.data/Views/EngagementView(General).cs
+++ contentapi.data/Views/EngagementView(General).cs
@@ -31,48 +31,45 @@ public class ContentEngagementView : IContentUserRelatedView, IEngagementView
     public long contentId { get; set; }
 
     //An alias
-    //[FieldSelect("contentId")]
-    [FullIgnore]
+    [DbField("contentId")]
     public long relatedId => contentId;
 
     public void SetRelatedId(long id) { contentId = id; }
 }
 
 [ResultFor(RequestType.message_engagement)]
-[SelectFrom("message_engagement")] // e join messages m on e.messageId = m.id")]
+[SelectFrom("message_engagement e join messages m on e.messageId = m.id")]
 [WriteAs(typeof(Db.MessageEngagement))]
 public class MessageEngagementView : IContentUserRelatedView, IEngagementView
 {
-    [DbField()] //"e.id", "id")]
+    [DbField("e.id", "e.id", "id")]
     public long id { get; set; }
 
-    [DbField()] //"e.userId", "userId")]
+    [DbField("e.userId", "e.userId", "userId")]
     [Writable(WriteRule.AutoUserId, WriteRule.Preserve)]
     public long userId { get; set; }
 
-    [DbField()] //"e.type", "type")]
+    [DbField("e.type", "e.type", "type")]
     [Writable]
     public string type { get; set; } = "";
 
-    [DbField()] //"e.engagement", "engagement")]
+    [DbField("e.engagement", "e.engagement", "engagement")]
     [Writable]
     public string engagement { get; set; } = "";
 
-    [DbField()] //"e.createDate", "createDate")]
+    [DbField("e.createDate", "e.createDate", "createDate")]
     [Writable(WriteRule.AutoDate, WriteRule.Preserve)]
     public DateTime createDate { get; set; }
 
-    [DbField()] //"e.messageId", "messageId")]
+    [DbField("e.messageId", "e.messageId", "messageId")]
     [Writable(WriteRule.User, WriteRule.Preserve)]
     public long messageId { get; set; }
     
-    [Expensive(1)]
-    [DbField("select contentId from messages m where m.id = messageId")] //"m.contentId", "contentId")]
+    [DbField("m.contentId", "m.contentId")] //No need to define write
     public long contentId {get;set;}
 
     //An alias
-    //[FieldSelect("messageId")]
-    [FullIgnore]
+    [DbField("e.messageId", "e.messageId")]
     public long relatedId => messageId;
 
     public void SetRelatedId(long id) { messageId = id; }
