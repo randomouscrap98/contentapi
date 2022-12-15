2cf22d648b0f59d501b039209af22b54bb0a3c57
Carlos Sanchez <randomouscrap98@aol.com>
2022-12-15 16:33:54 -0500

    Actually load querybuilder config

--- contentapi/DefaultSetup.cs
+++ contentapi/DefaultSetup.cs
@@ -113,6 +113,7 @@ public static class DefaultSetup
         services.AddSingleton<ModuleServiceConfig>();
         services.AddSingleton<EventTrackerConfig>();
         services.AddSingleton<FileServiceConfig>();
+        services.AddSingleton<QueryBuilderConfig>();
 
         //NOTE: do NOT just add all configs to the service! Only configs which have 
         //reasonable defaults! For instance: the EmailConfig should NOT be added!
