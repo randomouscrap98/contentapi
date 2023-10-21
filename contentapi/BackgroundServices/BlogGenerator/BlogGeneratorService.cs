57bfa22222432afeed5ac46498dd822c12849348
Carlos Sanchez <randomouscrap98@aol.com>
2023-10-21 18:05:55 -0400

    Maybe working blog generation?

--- contentapi/BackgroundServices/BlogGenerator/BlogGeneratorService.cs
+++ contentapi/BackgroundServices/BlogGenerator/BlogGeneratorService.cs
@@ -1,4 +1,6 @@
 
+using System.Diagnostics;
+using System.Security.Cryptography.Xml;
 using System.Text.RegularExpressions;
 using blog_generator;
 using contentapi.data;
@@ -50,6 +52,60 @@ public class BlogGeneratorService : BackgroundService
         this.templateLoader = templateLoader;
     }
 
+    public class BlogData
+    {
+        public ContentView Parent;
+        public List<ContentView> AllChildren;
+        public List<ActivityView> AllActivity;
+        public List<UserView> AllUsers;
+        private ILogger logger;
+
+        public BlogData(ContentView parent, List<ContentView> children, List<ActivityView> activity, List<UserView> users, ILogger logger)
+        {
+            Parent = parent;
+            AllChildren = children;
+            AllActivity = activity;
+            AllUsers = users;
+            this.logger = logger;
+        }
+
+        public List<ContentView> GetPages(bool includeParent = false)
+        {
+            var result = AllChildren.Where(x => x.literalType == RESOURCETYPE).ToList();
+            if(!result.Any(x => x.id == Parent.id) && includeParent)
+                result.Add(Parent);
+            return result;
+        }
+
+        public List<ContentView> GetStyles()
+        {
+            if(Parent.values.ContainsKey(STYLESVALUEKEY))
+            {
+                try
+                {
+                    var styles = blog_generator.Utilities.ForceCastResult<List<string>>(Parent.values[STYLESVALUEKEY]) ??
+                        throw new InvalidOperationException($"Couldn't cast {STYLESVALUEKEY} to list!");
+                    return AllChildren.Where(x => styles.Contains(x.hash)).ToList();
+                }
+                catch(Exception ex)
+                {
+                    logger.LogWarning($"Couldn't parse the parent styles for {Parent.hash}({Parent.id}): {ex}");
+                }
+            }
+            else
+            {
+                logger.LogDebug($"No styles for parent {Parent.hash}({Parent.id}) found when requested");
+            }
+
+            return new List<ContentView>();
+        }
+
+        public ActivityView? GetPageAcivity(ContentView page) => AllActivity.FirstOrDefault(x => x.id == page.lastRevisionId);
+        public string GetAuthorFromList(long id) => AllUsers.FirstOrDefault(x => x.id == id)?.username ?? "???";
+        public string GetPageFilename(ContentView page) => page.id == Parent.id ? INDEXFILE : page.hash + ".html";
+        public string GetStyleFilename(ContentView style) => style.hash + ".css";
+    }
+
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         if(config.Interval.Ticks == 0)
@@ -92,6 +148,7 @@ public class BlogGeneratorService : BackgroundService
 
     public string BlogPath(string hash) => Path.Join(config.BlogsFolder, hash);
     public string BlogPath(ContentView view) => Path.Join(config.BlogsFolder, view.hash);
+    public string BlogPath(BlogData data) => Path.Join(config.BlogsFolder, data.Parent.hash);
 
     public long GetBlogRevision(ContentView blog, List<ContentView> children)
     {
@@ -103,6 +160,7 @@ public class BlogGeneratorService : BackgroundService
 
         return maxId;
     }
+    public long GetBlogRevision(BlogData data) => GetBlogRevision(data.Parent, data.AllChildren);
 
     /// <summary>
     /// Given the base content and all related content, figure out if a blog should be regenerated
@@ -196,7 +254,9 @@ public class BlogGeneratorService : BackgroundService
                     { "ids", userIds.Distinct() }
                 });
 
-                await RegenerateBlog(await searcher.GetById<ContentView>(blog.id), fullChildren, activity, users);
+                await RegenerateBlog(
+                    new BlogData(await searcher.GetById<ContentView>(blog.id), fullChildren, activity, users, logger)
+                );
             }
             else
             {
@@ -211,9 +271,9 @@ public class BlogGeneratorService : BackgroundService
     /// <param name="blog"></param>
     /// <param name="children"></param>
     /// <returns></returns>
-    public async Task RegenerateBlog(ContentView blog, List<ContentView> children, List<ActivityView> activity, List<UserView> users)
+    public async Task RegenerateBlog(BlogData data)
     {
-        var basePath = BlogPath(blog);
+        var basePath = BlogPath(data);
 
         if(!Directory.Exists(basePath))
             Directory.CreateDirectory(basePath);
@@ -227,52 +287,89 @@ public class BlogGeneratorService : BackgroundService
             File.Copy(Path.Join(config.StaticFilesBase, localPath), destinationPath, true);
         }
 
-        //await RegenerateBlogPost(blog, blog, activity, users);
+        //Generate all the pages
+        foreach(var page in data.GetPages(includeParent:true))
+            await RegenerateBlogPost(data, page);
+        
+        //Generate the styles
+        foreach(var style in data.GetStyles())
+            await RegenerateStyle(data, style);
 
         // Only at the END do you write the revision file
-        var revisionId = GetBlogRevision(blog, children);
-        var revPath = Path.Join(BlogPath(blog), REVISIONFILE);
+        var revisionId = GetBlogRevision(data);
+        var revPath = Path.Join(BlogPath(data), REVISIONFILE);
         await File.WriteAllTextAsync(revPath, @$"{revisionId}");
     }
 
-    public string GetAuthorFromList(long id, IEnumerable<UserView> users) => users.FirstOrDefault(x => x.id == id)?.username ?? "???";
 
-    //public async Task RegenerateBlogPost(ContentView page, ContentView parent, List<ActivityView> activity, List<UserView> users)
-    //{
-    //    var revision = activity.FirstOrDefault(x => x.id == page.lastRevisionId);
+    /// <summary>
+    /// Generate a single page of a blog and ONLY the page
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="page"></param>
+    /// <returns></returns>
+    public async Task RegenerateBlogPost(BlogData data, ContentView page)
+    {
+        logger.LogDebug($"Generating blog post: {page.hash}({page.id})");
 
-    //    //This generates a single blogpost. It figures out how to generate it based on the data given. If the page itself IS the parent,
-    //    //something else MAY be done.
-    //    var templateData = new MainTemplateData()
-    //    {
-    //        scripts = config.ScriptIncludes,
-    //        styles = config.StyleIncludes,
-    //        page = page,
-    //        parent = parent,
-    //        render_date = DateTime.UtcNow,
-    //        version = (DateTime.Now.Ticks / TicksPerSecond).ToString(),
-    //        keywords = string.Join(", ", page.keywords.Union(parent.keywords)),
-    //        parent_link = pathManager.WebBlogMainPath(parent.hash),
-    //        author = GetAuthorFromList(page.createUserId, users),
-    //        edit_author = GetAuthorFromList(revision?.userId ?? -1, users),
-    //        revision = revision
-    //    };
+        var revision = data.GetPageAcivity(page);
+        var pages = data.GetPages();
+        var styles = data.GetStyles();
 
-    //    templateData.styles.AddRange(GetStylesForParent(parent).Select(x => pathManager.WebStylePath(x)));
+        //This generates a single blogpost. It figures out how to generate it based on the data given. If the page itself IS the parent,
+        //something else MAY be done.
+        var templateData = new MainTemplateData()
+        {
+            scripts = config.ScriptIncludes,
+            styles = config.StyleIncludes,
+            page = page,
+            parent = data.Parent,
+            render_date = DateTime.UtcNow,
+            version = (DateTime.Now.Ticks / TicksPerSecond).ToString(),
+            keywords = string.Join(", ", page.keywords.Union(data.Parent.keywords)),
+            parent_link = ".", //isRoot ? "." : "..", //pathManager.WebBlogMainPath(parent.hash),
+            author = data.GetAuthorFromList(page.createUserId),
+            edit_author = data.GetAuthorFromList(revision?.userId ?? -1),
+            revision = revision
+        };
 
-    //    templateData.navlinks = pages.OrderByDescending(x => x.createDate).Select(x => new NavigationItem()
-    //    {
-    //        text = x.name,
-    //        link = pathManager.WebBlogPagePath(parent.hash, x.hash),
-    //        current = x.id == page.id,
-    //        create_date = x.createDate,
-    //        hash = x.hash
-    //    }).ToList();
+        templateData.styles.AddRange(styles.Select(x => data.GetStyleFilename(x)));//GetStylesForParent(parent).Select(x => pathManager.WebStylePath(x)));
 
-    //    //Need to use mustache here to generate the template and write it
-    //    var renderedPage = await templateLoader.RenderPageAsync(MAINTEMPLATE, templateData);
+        templateData.navlinks = pages.OrderByDescending(x => x.createDate).Select(x => new NavigationItem()
+        {
+            text = x.name,
+            link = data.GetPageFilename(x), //x.hash + ".html",//pathManager.WebBlogPagePath(parent.hash, x.hash),
+            current = x.id == page.id,
+            create_date = x.createDate,
+            hash = x.hash
+        }).ToList();
 
-    //    var path = Path.Combine(basePath, page.id == parent.id ? INDEXFILE : pathManager.LocalBlogPagePath(parent.hash, page.hash);
-    //    await WriteAny(path, renderedPage, "page");
-    //}
+        //Need to use mustache here to generate the template and write it
+        var renderedPage = await templateLoader.RenderPageAsync(MAINTEMPLATE, templateData);
+        var basePath = BlogPath(data);
+
+        var path = Path.Join(basePath, data.GetPageFilename(page)); //page.id == parent.id ? INDEXFILE : pathManager.LocalBlogPagePath(parent.hash, page.hash);
+        await File.WriteAllTextAsync(path, renderedPage);
+        logger.LogInformation($"Wrote blog page {path}");
+    }
+
+    public async Task RegenerateStyle(BlogData data, ContentView style)
+    {
+        logger.LogDebug($"Generating style: {style.hash}({style.id})");
+
+        var templateData = new StyleData()
+        {
+            page = style,
+            render_date = DateTime.UtcNow,
+            author = data.GetAuthorFromList(style.createUserId)
+        };
+
+        //Need to use mustache here to generate the template and write it
+        var renderedPage = await templateLoader.RenderPageAsync(STYLETEMPLATE, templateData);
+        var basePath = BlogPath(data);
+
+        var path = Path.Join(basePath, data.GetStyleFilename(style)); //pathManager.LocalStylePath(style.hash);
+        await File.WriteAllTextAsync(path, renderedPage);
+        logger.LogInformation($"Wrote blog style {path}");
+    }
 }
